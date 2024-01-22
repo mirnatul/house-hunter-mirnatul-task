359ed032b24387956197c57b9290c728cea8942c
mirnatul <www.mirnatul@gmail.com>
2024-01-23 01:15:50 +0600

    added tailwind and daisyui also created Main and Dashboard layout and basic navbar, footer and homepage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,6 +11,8 @@ import { router } from './Routes/Router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div className='max-w-full mx-auto px-2 lg:px-10'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
